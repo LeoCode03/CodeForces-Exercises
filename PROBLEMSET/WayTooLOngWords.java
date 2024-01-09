9511df3705ada78fe0517219f2d037839e506f0b
Leandro Soliz <leandrosoliz310@gmail.com>
2024-01-09 12:07:47 -0400

    2do ejercicio de codeforces resuelto

---
+++ PROBLEMSET/WayTooLOngWords.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class WayTooLOngWords
+{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int words = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < words; i++) {
+            String word = sc.nextLine();
+            System.out.println(shortWord(word));
+        }
+    }
+
+    private static String shortWord(String word) {
+        if(word.length() > 10) {
+            return word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
+        }else {
+            return word;
+        }
+    }
+}
